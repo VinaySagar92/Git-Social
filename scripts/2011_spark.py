5ea34c91e45d9d37692b86a33702e353b51d2bd8
Ubuntu <ubuntu@ip-10-0-0-7.us-west-2.compute.internal>
2018-08-14 02:17:58 +0000

    Version 8/13/2018

--- scripts/2011_spark.py
+++ scripts/2011_spark.py
@@ -8,6 +8,7 @@ import random
 from cassandra.cluster import Cluster
 import os
 import pyspark_cassandra
+from datetime import datetime
 
 # Creating a Cluster object to connect to Cassandra cluster and keyspace
 cluster = Cluster(['52.43.163.255', '52.11.91.29', '54.70.77.167', '54.69.253.190'])
@@ -18,7 +19,7 @@ spark = SparkSession.builder \
             .appName("S3 READ TEST") \
             .config("spark.executor.cores", "6") \
             .config("spark.executor.memory", "6gb") \
-            .getOrCreate()
+	    .getOrCreate()
 
 sc=spark.sparkContext
 sqlContext = SQLContext(sc)
@@ -30,8 +31,8 @@ hadoop_conf.set("fs.s3a.awsAccessKeyId", os.environ['AWS_ACCESS_KEY_ID'])
 hadoop_conf.set("fs.s3a.awsSecretAccessKey", os.environ['AWS_SECRET_ACCESS_KEY'])
 
 # reading events data for 2011 from S3
-df11 = spark.read.json("s3a://vinaysagar-bucket/2017/2017-*.json.gz")
-#df11 = spark.read.json("/usr/local/spark/2011.json.gz")
+df11 = spark.read.json("s3a://vinaysagar-bucket/2011/2011-*.json.gz")
+#df11 = spark.read.json("s3a://vinaysagar-bucket/2011/2011-000000000000.json.gz")
 
 # filtering rows with just the three relevant events
 df11_watch = df11.filter("type='WatchEvent'")
@@ -44,9 +45,11 @@ sqlContext.registerDataFrameAsTable(df11_commit, "df11_commit_table")
 sqlContext.registerDataFrameAsTable(df11_fork, "df11_fork_table")
 
 # creating new dataframes with just the relevant columns
-df11_watch_altered = sqlContext.sql("SELECT actor, repo, type FROM df11_watch_table")
-df11_commit_altered = sqlContext.sql("SELECT actor, repo, type FROM df11_commit_table")
-df11_fork_altered = sqlContext.sql("SELECT actor, repo, type FROM df11_fork_table")
+df11_watch_altered = sqlContext.sql("SELECT actor, repo, type, created_at FROM df11_watch_table WHERE actor IS NOT NULL and created_at IS NOT NULL and repo IS NOT NULL")
+df11_commit_altered = sqlContext.sql("SELECT actor, repo, type, created_at  FROM df11_commit_table WHERE actor is NOT NULL and created_at IS NOT NULL and repo IS NOT NULL")
+df11_fork_altered = sqlContext.sql("SELECT actor, repo, type, created_at FROM df11_fork_table WHERE actor is NOT NULL and created_at IS NOT NULL and repo IS NOT NULL")
+
+#df11_watch_alter = sqlContext.sql("SELECT actor, repo, type, created_at FROM df11_watch_table WHERE actor IS NULL and created_at IS NOT NULL")
 
 # registering dataframes as tables to get a union of all
 sqlContext.registerDataFrameAsTable(df11_watch_altered, "df11_watch_altered_table")
@@ -57,65 +60,92 @@ sqlContext.registerDataFrameAsTable(df11_fork_altered, "df11_fork_altered_table"
 df11_altered_union = sqlContext.sql("SELECT * from df11_watch_altered_table UNION ALL SELECT * from df11_commit_altered_table UNION ALL SELECT * from df11_fork_altered_table")
 sqlContext.registerDataFrameAsTable(df11_altered_union, "df11_altered_union_table")
 
+
+def splitRepo(a):
+	b = datetime.strptime(a.created_at.split(" ")[0], '%Y-%m-%d')
+	return ((a.actor.login, b), a.repo.name)
+
+def splitTopic(a):
+	b = datetime.strptime(a.time.split(" ")[0], '%Y-%m-%d')
+	return ((a.user, b), a.topic)
+
+def splitUser(a):
+	b = datetime.strptime(a.time.split(" ")[0], '%Y-%m-%d')
+	return ((a.topic, b), a.user)
+
+def comb(a):
+	return [a]
+
+def merg(a, b):
+	a.append(b)
+	return a
+
+def mergComb(a, b):
+	a.extend(b)
+	return a
+
 # getting all the user to repo one-to-one mappings
-#user_repo_map11 = df11_altered_union.rdd.map(lambda x: {"user": x.actor.login, "repo": x.repo.name}).toDF()
+user_repo_map11 = df11_altered_union.rdd.map(lambda x: {"user": x.actor.login, "time": x.created_at, "repo": x.repo.name}).toDF()
+user_repo_mapN = user_repo_map11.na.drop(subset=('user', 'time')).rdd.map(lambda c: {"repo": c[0], "time": c[1], "user": c[2]}).toDF()
+
+print(user_repo_mapN.show())
 
 ### Repo to Topic to be used for user to topic mapping
 #Topics that are available are given as a list
-#top = ['Java', 'Python', 'API', 'Javascript', 'Ruby', 'Ruby on Rails', 'Scala', 'REST', 'JAX-RS', 'HTML', 'CSS', 'PHP']
+top = ['3D', 'Ajax', 'Algorithm', 'Amp', 'Android', 'Angular', 'Ansible', 'API', 'Adruino', 'ASP.NET', 'Atom', 'Awesome Lists', 'Amazon Web Services', 'Azure', 'Babel', 'Blockchain', 'Bootstrap', 'Bot', 'C', 'Chrome', 'Chrome extension', 'Command line interface', 'Clojure', 'Code quality', 'Code review', 'Compiler', 'Coninuous integration', 'C++', 'Cryptocurrency', 'Crystal', 'C#', 'CSS', 'Data structures', 'Data visualization', 'Database', 'Deep leaning', 'Dependency management', 'Deployment', 'Django', 'Docker', 'Documentation', '.NET', 'Electron', 'Elixir', 'Emacs', 'Ember', 'Emoji', 'Emulator', 'ES6', 'ESLint', 'Ethereum', 'Express', 'Firebase', 'Firefox', 'Flask', 'Font', 'Framework', 'Front end', 'Game engine', 'Git', 'GitHub API', 'Go', 'Google', 'Gradle', 'GraphQL', 'Gulp', 'Haskell', 'Homebrew', 'Homebridge', 'HTML', 'HTTP', 'Icon font', 'iOS', 'IPFS', 'Java', 'Javascript', 'Jekyll', 'jQuery', 'JSON', 'The Julia Language', 'Jupyter Notebook', 'Koa', 'Kotlin', 'Kubernetes', 'Laravel', 'LaTex', 'Library', 'Linux', 'Localization', 'Lua', 'Machine learning', 'macOS', 'Markdown', 'Mastodon', 'Material design', 'MATLAB', 'Maven', 'Minecraft', 'Mobile', 'Monero', 'MongoDB', 'Mongoose', 'Monitoring', 'MvvmCross', 'MySQL', 'NativeScript', 'Nim', 'Natural language processing', 'Node.js', 'NoSQL', 'npm', 'Objective-C', 'OpenGL', 'Operating System', 'P2P', 'Package manager', 'Language parsing', 'Perl', 'Perl 6', 'Phaser', 'PHP', 'Pixel Art', 'PostgreSQL', 'Project management', 'Publishing', 'PWA', 'Python', 'Qt', 'R', 'Rails', 'Raspberry Pi', 'Ratchet', 'React', 'React Native', 'ReactiveUI', 'Redux', 'REST API', 'Ruby', 'Rust', 'Sass', 'Scala', 'scikit-learn', 'Software-defined networking', 'Security', 'Server', 'Serverless', 'Shell', 'Sketch', 'SpaceVim', 'Spring Boot', 'SQL', 'Storybook', 'Support', 'Swift', 'Symfony', 'Telegram', 'Tensorflow', 'Terminal', 'Terraform', 'Twitter', 'Typescript', 'Ubuntu', 'Unity', 'Unreal Engine', 'Vagrant', 'Vim', 'Virtual Reality', 'Vue.js', 'Wagtail', 'Web Components', 'Web App', 'Webpack', 'Windows', 'Wodplate', 'Wordpress', 'Xamarin', 'XML']
 # Creating a RDD by mapping repos with 5 topics from the list of topics
-#df11_altered = sqlContext.sql("SELECT repo FROM df11_altered_union_table")
-#repo_topic_map = df11_altered.rdd.map(lambda x: (x.repo.name, random.sample(top, 5)))
+df11_altered = sqlContext.sql("SELECT repo FROM df11_altered_union_table WHERE repo IS NOT NULL")
+repo_topic_map = df11_altered.rdd.map(lambda x: (x.repo.name, random.sample(top, 5)))
 # Creating the dataframe with topics as a list
-#repo_topic = repo_topic_map.map(lambda x: {"repo":x[0], "topic":([user for sublist in x[1] for user in sublist])}).toDF()
+repo_topic = repo_topic_map.map(lambda x: {"repo":x[0], "topic":([user for sublist in x[1] for user in sublist])}).toDF()
+
+
 
 # Performing an inner join for the user to topic relation on the two dataframes created
-#df_join = user_repo_map11.join(repo_topic.select(repo_topic.repo, explode(repo_topic.topic).alias("topics")), user_repo_map11.repo == repo_topic.repo).toDF("repo", "user", "repo", "topic").select("user", "topic")
+df_join = user_repo_mapN.join(repo_topic.select(repo_topic.repo, explode(repo_topic.topic).alias("topics")), user_repo_mapN.repo == repo_topic.repo).toDF("repo", "time", "user", "repo", "topic").select("user", "time", "topic")
+
 
 ### User to User Mapping
 # creating a list of users to make a mapping
-#df11_user = sqlContext.sql("SELECT actor FROM df11_altered_union_table")
-#user_map = df11_user.rdd.map(lambda x: {"user": x.actor.login}).toDF()
-#user_list = [i.user for i in user_map.collect()]
+df11_user = sqlContext.sql("SELECT actor FROM df11_altered_union_table where actor is NOT NULL")
+user_map = df11_user.rdd.map(lambda x: {"user": x.actor.login}).toDF().dropna(subset='user')
+
+user_list = [i.user for i in user_map.collect()]
 # mapping users to follow 5 other users
-#user_user_map = df11_user.rdd.map(lambda x: (x.actor.login, random.sample(user_list, 5))).groupByKey()
+user_user_map = df11_user.rdd.map(lambda x: (x.actor.login, random.sample(user_list, 5))).groupByKey()
 # collecting the pipelined RDD as a list to be written to casandra table
-#user_user = user_user_map.map(lambda x: {"user":x[0], "userList":([user for sublist in x[1] for user in sublist])}).collect()
+user_user = user_user_map.map(lambda x: {"username":x[0], "userfollow":([user for sublist in x[1] for user in sublist])})
+user_user_db = user_user.toDF().dropna().rdd.map(lambda c: ((c[1], c[0])))
+# writing to cassandra table useruser
+user_user_db.saveToCassandra("events", "useruser")
+
 
 ### User to Repo Mapping
 # grouping all records for a given username to get all repositories that the user is following and has contributed to
-user_repo_map = df11_altered_union.rdd.map(lambda x: (x.actor.login, list([x.repo.name]))).groupByKey()
-# collecting the pipelined RDD as a list to be written to casandra table
-user_repo = user_repo_map.map(lambda x: {"username":x[0], "repo":[user for sublist in x[1] for user in sublist]})
+user_repo_map = df11_altered_union.rdd.map(splitRepo).combineByKey(comb, merg, mergComb).map(lambda c: ((c[0][0], c[0][1], c[1])))
+user_rep = user_repo_map.toDF().na.drop(subset=('_1', '_2')).rdd.map(lambda c: ((c[0], c[1], c[2])))
+# writing to cassandra table userrepo
+user_rep.saveToCassandra("events", "userrepo")
 
-user_repo.saveToCassandra("events", "userrepo2011")
 
 ### User to Topic Mapping
 # grouping all records for a given username to get all topics that the user is following and has contributed to
-#user_topic_map11 = df_join.rdd.map(lambda x: (x.user, list([x.topic]))).groupByKey()
-# collecting the pipelined RDD as a list to be written to casandra table
-#user_topic11 = user_topic_map11.map(lambda x: {"username":x[0], "topic":[user for sublist in x[1] for user in sublist]}).collect()
+user_topic_map = df_join.rdd.map(splitTopic).combineByKey(comb, merg, mergComb).map(lambda c: ((c[0][0], c[0][1], c[1])))
+user_topic_db = user_topic_map.toDF().na.drop(subset=('_1', '_2')).rdd.map(lambda c: ((c[0], c[1], c[2])))
+# writing to cassandra table usertopic
+user_topic_db.saveToCassandra("events", "usertopic")
+
 
 ### Topic to User Mapping
 # grouping all records for a given topic to get all users who are following and has contributed for
-#topic_user_map11 = df_join.rdd.map(lambda x: (x.topic, list([x.user]))).groupByKey()
-# collecting the pipelined RDD as a list to be written to casandra table
-#topic_user11 = topic_user_map11.map(lambda x: {"topic":x[0], "username":[user for sublist in x[1] for user in sublist]}).collect()
+topic_user_map = df_join.rdd.map(splitUser).combineByKey(comb, merg, mergComb).map(lambda c: ((c[0][0], c[0][1], c[1])))
+topic_user_db = topic_user_map.toDF().na.drop(subset=('_1', '_2')).rdd.map(lambda c: ((c[0], c[1], c[2])))
+#print(topic_user_db.toDF().show())
+# writing to cassandra table topicuser
+topic_user_db.saveToCassandra("events", "topicuser")
 
-### Writing to Cassandra Tables
-
-
-
-# writing all values to cassandra table "userrepo2011"
-#for val in user_user:
+#for val in user_user_db:
 #  try:
-#    session.execute(
-#        """
-#        INSERT INTO userrepo2011(username, repo)
-#        VALUES (%s, %s)
-#        """,
-#        (val['username'], val['repo'])
-#        )
 #    print val
 #  except Exception as e:
 #    print e, val
